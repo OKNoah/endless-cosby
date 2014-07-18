a1ffd1b4f9acf9ebe90858f74651507f1d15aca1
Github-Noah <noahgray@me.com>
2014-07-18 13:11:29 -0700

    Version number added
    Version number added to allow Composer update

--- app/libraries/functions.php
+++ app/libraries/functions.php
@@ -2,13 +2,14 @@
 
 	class Cosby {
 
-		/* Get a row of Cosby */
+		/* Get a single Bill */
 		static function single($no) {
 			echo "<div class='col-md-3 well'>";
 			echo "<img class='center' src='http://www.placecosby.com/200/200/?image=" . $no . "'>";
 			echo "</div>";
 		}
 
+		/* Make a whole row of Cosbys. Accepts column count argument, but 4 is default and reccomended. */
 		static function row($col = 4) {
 			echo "<div class='row'>";
 			for ($i = 0; $i < $col; $i++) {
